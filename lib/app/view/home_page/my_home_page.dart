5fb65cc20bbf69f714b3f0283ce501afb3a70f6b
tugcenaz <nazlioglutugce@gmail.com>
2023-07-17 16:49:40 +0300

    locationWidget

--- lib/app/view/home_page/my_home_page.dart
+++ lib/app/view/home_page/my_home_page.dart
@@ -1,13 +1,60 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bounceable/flutter_bounceable.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:weather_app/core/constants/icon_constants.dart';
+import 'package:weather_app/core/styles/text_styles.dart';
 import '../../components/page_background_widget.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   Widget _buildBody() {
-    return const Column(
-      children: [],
+    return Column(
+      children: [
+        locationWidget(),
+        Text('ikonlar olacak'),
+
+      ],
+    );
+  }
+
+  Widget locationWidget() {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 70.0.sp, horizontal: 30.sp),
+      child: Bounceable(
+        onTap: () {
+          // Get.to(() => SearchPage());
+        },
+        child: Row(
+          children: [
+            SizedBox(
+              width: 24.sp,
+              height: 24.sp,
+              child: SvgPicture.asset(
+                IconConstants.map_icon,
+              ),
+            ),
+            SizedBox(
+              width: 20.sp,
+            ),
+            Text(
+              'Surabaya',
+              style: TextStyles.generalWhiteTextStyle1(),
+            ),
+            SizedBox(
+              width: 20.sp,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 4.0.sp),
+              child: SvgPicture.asset(
+                IconConstants.opt_icon,
+                width: 26.sp,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
