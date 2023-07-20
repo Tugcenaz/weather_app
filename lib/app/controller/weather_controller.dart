220bd800ac7e22e6d10f25ae1a3164f611cd0026
tugcenaz <nazlioglutugce@gmail.com>
2023-07-20 18:30:27 +0300

    update

--- lib/app/controller/weather_controller.dart
+++ lib/app/controller/weather_controller.dart
@@ -1,10 +1,13 @@
 import 'package:get/get.dart';
+import 'package:weather_app/app/controller/forecast_weather_controller.dart';
 
+import '../../core/time_convert.dart';
 import '../models/current_weather_model.dart';
 import '../services/weather_service.dart';
 
 class WeatherController extends GetxController {
   WeatherService weatherService = Get.find();
+  ForecastWeatherController forecastWeatherController = Get.find();
   final Rx<CurrentWeatherModel> _currentWeatherModel =
       CurrentWeatherModel().obs;
 
@@ -14,11 +17,15 @@ class WeatherController extends GetxController {
     _currentWeatherModel.value = value;
   }
 
-  Future<CurrentWeatherModel?> updateCurrentWeather({required String cityName}) async {
-    CurrentWeatherModel? result = await weatherService.getCurrentWeather(cityName: cityName);
+  Future<CurrentWeatherModel?> updateCurrentWeather(
+      {required String cityName}) async {
+    CurrentWeatherModel? result =
+        await weatherService.getCurrentWeather(cityName: cityName);
     if (result != null) {
       currentWeatherModel = result;
     }
     return currentWeatherModel;
   }
+
+
 }
