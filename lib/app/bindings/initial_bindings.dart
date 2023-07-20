601ae86ef30887c735b368cc94aa0fd86d0600cf
tugcenaz <nazlioglutugce@gmail.com>
2023-07-20 18:34:54 +0300

    forecast_weather_controller ve forecast_weather_service initialbindingse eklendi

--- lib/app/bindings/initial_bindings.dart
+++ lib/app/bindings/initial_bindings.dart
@@ -1,8 +1,10 @@
 import 'package:get/get.dart';
+import 'package:weather_app/app/controller/forecast_weather_controller.dart';
 import 'package:weather_app/app/controller/weather_controller.dart';
+import 'package:weather_app/app/services/forecast_weather_service.dart';
 import 'package:weather_app/app/services/search_service.dart';
 import '../controller/api_client_controller.dart';
-import '../controller/search_controller.dart';
+import '../controller/city_search_controller.dart';
 import '../services/weather_service.dart';
 
 class InitialBindings implements Bindings{
@@ -13,6 +15,8 @@ class InitialBindings implements Bindings{
    Get.lazyPut(() => CitySearchController(),fenix: true);
    Get.lazyPut(() => WeatherController(),fenix: true);
    Get.lazyPut(() => SearchService(),fenix: true);
+   Get.lazyPut(() => ForecastWeatherService(),fenix: true);
+   Get.lazyPut(() => ForecastWeatherController(),fenix: true);
   }
 
 }
