a12bdec3928afe9050cefc678320c349dfc9135b
tugcenaz <nazlioglutugce@gmail.com>
2023-07-20 18:29:57 +0300

    apiden gelen iconları eklemek için cachednetworkimage kullanılarak bir widget oluşturuldu

--- lib/app/components/weather_icon_widget.dart
+++ lib/app/components/weather_icon_widget.dart
@@ -2,10 +2,10 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-Widget weatherIconWidget({required String imageUrl}) {
+Widget weatherIconWidget({required String imageUrl,required double width,required double height}) {
   return CachedNetworkImage(
-    imageUrl: imageUrl,width: 200.w,height: 200.h,fit: BoxFit.fill,
-    placeholder: (context, url) => Center(child: const CircularProgressIndicator()),
+    imageUrl: imageUrl,width: width.w,height: height.h,fit: BoxFit.fill,
+    placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
     errorWidget: (context, url, error) => const Icon(Icons.error),
   );
 }
